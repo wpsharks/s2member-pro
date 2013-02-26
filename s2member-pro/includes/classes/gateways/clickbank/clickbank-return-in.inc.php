1bf471f1faca3b7999cef448352d6be787afd500
JasWSInc <jason@websharks-inc.com>
2013-02-26 02:09:06 -0900

    Updating ClickBank API Integration to v1.3
    * Updating ClickBank API integration to v1.3. Closes WebSharks/Private#56

--- s2member-pro/includes/classes/gateways/clickbank/clickbank-return-in.inc.php
+++ s2member-pro/includes/classes/gateways/clickbank/clickbank-return-in.inc.php
@@ -68,7 +68,7 @@ if(!class_exists("c_ws_plugin__s2member_pro_clickbank_return_in"))
 										sleep /* Sleep here to give ClickBank® a chance to finalize processing. Allows the API call to succeed. */(5);
 										$clickbank["s2member_log"][] = "Awake. It's ".date("D M j, Y g:i:s a T").". Processing will continue.";
 
-										if(is_array($order = json_decode(c_ws_plugin__s2member_utils_urls::remote("https://api.clickbank.com/rest/1.2/orders/".$clickbank["cbreceipt"], false, array_merge(c_ws_plugin__s2member_pro_clickbank_utilities::clickbank_api_headers(), array("timeout" => 20))), true)) && ($order = $order["orderData"]))
+										if(is_array($order = json_decode(c_ws_plugin__s2member_utils_urls::remote("https://api.clickbank.com/rest/1.3/orders/".$clickbank["cbreceipt"], false, array_merge(c_ws_plugin__s2member_pro_clickbank_utilities::clickbank_api_headers(), array("timeout" => 20))), true)) && ($order = $order["orderData"]))
 											{
 												$clickbank["s2member_log"][] = "Order API variables have been obtained from ClickBank®.";
 
