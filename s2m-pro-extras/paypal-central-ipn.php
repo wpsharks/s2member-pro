6883c44738626bc93c3e06873de5ae7604de691a
JasWSInc <jason@websharks-inc.com>
2013-10-25 15:59:23 -0800

    * (s2Member) **WordPress v3.7 Compatibility** s2Member further updated to support subtle changes in the WordPress v3.7 `wp-login.php` file. This release corrects a minor issue w/ patches applied by s2Member when running in a Multisite Network environment. If you are running s2Member on a Multisite Network, please be sure to run the automatic patcher provided in your Dashboard against WordPress v3.7 after updating to this release of s2Member. * (s2Member Pro) **PayPal Central IPN** Updating the example file: `s2m-pro-extras/paypal-central-ipn.php` to use one of the latest PayPal IP addresses listed [here](https://ppmts.custhelp.com/app/answers/detail/a_id/92).

--- s2m-pro-extras/paypal-central-ipn.php
+++ s2m-pro-extras/paypal-central-ipn.php
@@ -50,7 +50,7 @@ $_p = trim_deep /* Now trim this array deeply. */($_p);
 
 if ((preg_match("/^(.+?)(?:\||$)/i", (string)@$_p["custom"], $_m) || preg_match("/~(.+?)~/i", (string)@$_p["rp_invoice_id"], $_m)) && !empty($config[$_m[1]]))
 	{
-		$_paypal_ipn_server_ip = $_ip = /* Forge IP address to match the PayPal IPN server here. */ "216.113.188.202";
+		$_paypal_ipn_server_ip = $_ip = /* Forge IP address to match the PayPal IPN server here. */ "173.0.81.1";
 		// See list of IPs here: <https://cms.paypal.com/us/cgi-bin/?cmd=_render-content&content_ID=developer/howto_api_golivechecklist>.
 
 		echo (trim(curlpsr(($_url = $config[$_m[1]]), http_build_query($_p, null, "&"), 20, 20, array
