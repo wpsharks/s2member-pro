bea23f7f7a87da9ff738f162d8f27dba93e62e7e
JasWSInc <jason@websharks-inc.com>
2014-03-28 01:08:21 -0800

    Adding s2Drip shortcode by @clavaque. See also WebSharks/s2Member#53

--- s2member-pro/includes/codes.inc.php
+++ s2member-pro/includes/codes.inc.php
@@ -35,4 +35,5 @@ if(realpath(__FILE__) === realpath($_SERVER["SCRIPT_FILENAME"]))
 /*
 Add WordPress Editor Shortcodes.
 */
+add_shortcode("s2Drip", "c_ws_plugin__s2member_pro_sc_drip::shortcode");
 ?>
