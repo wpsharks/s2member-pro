ec2eb06283b408c5fbfee5485d19a77d4aa6e5a7
JasWSInc <jason@websharks-inc.com>
2014-03-28 01:10:12 -0800

    Formatting s2Drip file.

--- s2member-pro/includes/classes/sc-drip-in.inc.php
+++ s2member-pro/includes/classes/sc-drip-in.inc.php
@@ -1,83 +1,84 @@
 <?php
 /**
-* [s2Drip] Shortcode.
-*
-* Copyright: © 2009-2011
-* {@link http://www.websharks-inc.com/ WebSharks, Inc.}
-* (coded in the USA)
-*
-* This WordPress plugin (s2Member Pro) is comprised of two parts:
-*
-* o (1) Its PHP code is licensed under the GPL license, as is WordPress.
-* 	You should have received a copy of the GNU General Public License,
-* 	along with this software. In the main directory, see: /licensing/
-* 	If not, see: {@link http://www.gnu.org/licenses/}.
-*
-* o (2) All other parts of (s2Member Pro); including, but not limited to:
-* 	the CSS code, some JavaScript code, images, and design;
-* 	are licensed according to the license purchased.
-* 	See: {@link http://www.s2member.com/prices/}
-*
-* Unless you have our prior written consent, you must NOT directly or indirectly license,
-* sub-license, sell, resell, or provide for free; part (2) of the s2Member Pro Module;
-* or make an offer to do any of these things. All of these things are strictly
-* prohibited with part (2) of the s2Member Pro Module.
-*
-* Your purchase of s2Member Pro includes free lifetime upgrades via s2Member.com
-* (i.e. new features, bug fixes, updates, improvements); along with full access
-* to our video tutorial library: {@link http://www.s2member.com/videos/}
-*
-* @package s2Member\Shortcodes
-* @since 140328
-*/
-if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
+ * [s2Drip] Shortcode.
+ *
+ * Copyright: © 2009-2011
+ * {@link http://www.websharks-inc.com/ WebSharks, Inc.}
+ * (coded in the USA)
+ *
+ * This WordPress plugin (s2Member Pro) is comprised of two parts:
+ *
+ * o (1) Its PHP code is licensed under the GPL license, as is WordPress.
+ *   You should have received a copy of the GNU General Public License,
+ *   along with this software. In the main directory, see: /licensing/
+ *   If not, see: {@link http://www.gnu.org/licenses/}.
+ *
+ * o (2) All other parts of (s2Member Pro); including, but not limited to:
+ *   the CSS code, some JavaScript code, images, and design;
+ *   are licensed according to the license purchased.
+ *   See: {@link http://www.s2member.com/prices/}
+ *
+ * Unless you have our prior written consent, you must NOT directly or indirectly license,
+ * sub-license, sell, resell, or provide for free; part (2) of the s2Member Pro Module;
+ * or make an offer to do any of these things. All of these things are strictly
+ * prohibited with part (2) of the s2Member Pro Module.
+ *
+ * Your purchase of s2Member Pro includes free lifetime upgrades via s2Member.com
+ * (i.e. new features, bug fixes, updates, improvements); along with full access
+ * to our video tutorial library: {@link http://www.s2member.com/videos/}
+ *
+ * @package s2Member\Shortcodes
+ * @since 140328
+ */
+if(realpath(__FILE__) === realpath($_SERVER["SCRIPT_FILENAME"]))
 	exit("Do not access this file directly.");
 
-if (!class_exists ("c_ws_plugin__s2member_pro_sc_drip_in"))
+if(!class_exists("c_ws_plugin__s2member_pro_sc_drip_in"))
 	{
 		/**
-		* [s2Drip] Shortcode.
-		*
-		* @package s2Member\Shortcodes
-		* @since 140328
-		*/
+		 * [s2Drip] Shortcode.
+		 *
+		 * @package s2Member\Shortcodes
+		 * @since 140328
+		 */
 		class c_ws_plugin__s2member_pro_sc_drip_in
-			{
-				/**
-				* [s2Drip] Shortcode.
-				*
-				* @package s2Member\Shortcodes
-				* @since 140328
-				*
-				* @attaches-to ``add_shortcode("s2Drip");``
-				*
-				* @param array $attr An array of Attributes.
-				* @param str $content Content inside the Shortcode.
-				* @param str $shortcode The actual Shortcode name itself.
-				* @return inner Return-value of inner routine.
-				*/
-				public static function shortcode ($attr = FALSE, $content = FALSE, $shortcode = FALSE)
-					{
-			        $drip = false;
-			        shortcode_atts(array("level" => "0", "after_day" => "0", "before_day" => NULL), $attr, $shortcode);
-			        $attr["level"] = (integer)$attr["level"]; // Non-integers become `0` here.
+		{
+			/**
+			 * [s2Drip] Shortcode.
+			 *
+			 * @package s2Member\Shortcodes
+			 * @since 140328
+			 *
+			 * @attaches-to ``add_shortcode("s2Drip");``
+			 *
+			 * @param array $attr An array of Attributes.
+			 * @param str   $content Content inside the Shortcode.
+			 * @param str   $shortcode The actual Shortcode name itself.
+			 *
+			 * @return inner Return-value of inner routine.
+			 */
+			public static function shortcode($attr = FALSE, $content = FALSE, $shortcode = FALSE)
+				{
+					$drip = FALSE;
+					shortcode_atts(array("level" => "0", "after_day" => "0", "before_day" => NULL), $attr, $shortcode);
+					$attr["level"] = (integer)$attr["level"]; // Non-integers become `0` here.
 
-			        if (current_user_can("access_s2member_level".$attr["level"]))
-			        {
-			            $level_time = (!$attr["level"])
-				            ? c_ws_plugin__s2member_registration_times::registration_time()
-				            : c_ws_plugin__s2member_registration_times::paid_registration_time("level".$attr["level"]);
+					if(current_user_can("access_s2member_level".$attr["level"]))
+						{
+							$level_time = (!$attr["level"])
+								? c_ws_plugin__s2member_registration_times::registration_time()
+								: c_ws_plugin__s2member_registration_times::paid_registration_time("level".$attr["level"]);
 
-			            $time = time(); // UTC time; including times given by s2Member (all UTC).
-			            if ($time > ($level_time + ((int)$attr["after_day"] * 86400)))
-			            {
-			                $drip = true;
-			                if (isset($attr["before_day"]) && $time > ($level_time + ((int)$attr["before_day"] * 86400)))
-			                    $drip = false;
-			            }
-			        }
-			        return $drip ? $content : "";
-					}
-			}
+							$time = time(); // UTC time; including times given by s2Member (all UTC).
+							if($time > ($level_time + ((int)$attr["after_day"] * 86400)))
+								{
+									$drip = TRUE;
+									if(isset($attr["before_day"]) && $time > ($level_time + ((int)$attr["before_day"] * 86400)))
+										$drip = FALSE;
+								}
+						}
+					return $drip ? $content : "";
+				}
+		}
 	}
 ?>
