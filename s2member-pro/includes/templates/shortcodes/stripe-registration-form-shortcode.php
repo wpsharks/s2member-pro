3b5d45afb9db90dc8f4cc701eb596960433110da
JasWSInc <jason@websharks-inc.com>
2014-07-10 03:35:10 -0800

    Working to resolve websharks/s2member#228

--- s2member-pro/includes/templates/shortcodes/stripe-registration-form-shortcode.php
+++ s2member-pro/includes/templates/shortcodes/stripe-registration-form-shortcode.php
@@ -3,4 +3,4 @@ if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
 	exit("Do not access this file directly.");
 ?>
 
-[s2Member-Pro-Stripe-Form register="1" level="%%level%%" ccaps="" desc="<?php echo esc_attr (_x ("Signup now, it's Free!", "s2member-front", "s2member")); ?>" custom="%%custom%%" tp="0" tt="D" captcha="clean" /]
+[s2Member-Pro-Stripe-Form register="1" level="%%level%%" ccaps="" desc="<?php echo _x ("Signup now, it's Free!", "s2member-front", "s2member"); ?>" custom="%%custom%%" tp="0" tt="D" captcha="clean" /]
