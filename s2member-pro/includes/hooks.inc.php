48c8033986cbcf2767ed46bd12ce748eb2b27488
JasWSInc <jas@wsharks.com>
2014-08-14 02:51:12 -0800

    Support SMIL files. See: websharks/s2member#179

--- s2member-pro/includes/hooks.inc.php
+++ s2member-pro/includes/hooks.inc.php
@@ -35,6 +35,7 @@ if(realpath(__FILE__) === realpath($_SERVER['SCRIPT_FILENAME']))
 /*
 Add the plugin Actions/Filters here.
 */
+add_action('init', 'c_ws_plugin__s2member_pro_rsf_files::serve', 3);
 add_action('init', 'c_ws_plugin__s2member_pro_exports::export', 3);
 add_action('init', 'c_ws_plugin__s2member_pro_exports_simple::export', 3);
 add_action('init', 'c_ws_plugin__s2member_pro_remote_ops::remote_ops');
