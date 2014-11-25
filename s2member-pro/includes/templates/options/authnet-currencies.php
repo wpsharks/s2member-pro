a3c2142696d195e96120047115e6ac6f58af9de2
JasWSInc <jas@wsharks.com>
2014-11-24 22:44:12 -0900

    Adding `AUD` as a valid Authorize.Net currency code. https://github.com/websharks/s2member/issues/383

--- s2member-pro/includes/templates/options/authnet-currencies.php
+++ s2member-pro/includes/templates/options/authnet-currencies.php
@@ -5,6 +5,7 @@ if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
 
 <optgroup label="<?php echo esc_attr (_x ("Currency", "s2member-admin", "s2member")); ?>">
 <option value="USD" selected="selected">USD</option>
+<option value="AUD">AUD</option>
 <option value="CAD">CAD</option>
 <option value="EUR">EUR</option>
 <option value="GBP">GBP</option>
