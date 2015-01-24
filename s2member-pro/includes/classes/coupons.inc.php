8d8d971ff01f5cf5cb567353f6814682762416c7
jaswsinc <jas@wsharks.com>
2015-01-23 23:31:43 -0900

    Getters/setters for coupon usage tracking. See: websharks/s2member#285, websharks/s2member#403, websharks/s2member#386

--- s2member-pro/includes/classes/coupons.inc.php
+++ s2member-pro/includes/classes/coupons.inc.php
@@ -43,9 +43,9 @@ if(!class_exists('c_ws_plugin__s2member_pro_coupons'))
 	 */
 	class c_ws_plugin__s2member_pro_coupons
 	{
-		protected $list = '';
+		public $list = '';
 
-		protected $coupons = array();
+		public $coupons = array();
 
 		public function __construct($args = array())
 		{
@@ -56,7 +56,7 @@ if(!class_exists('c_ws_plugin__s2member_pro_coupons'))
 			$this->coupons = $this->list_to_coupons($GLOBALS['WS_PLUGIN__']['s2member']['o']['pro_coupon_codes']);
 		}
 
-		protected function list_to_coupons($list, $update = TRUE)
+		public function list_to_coupons($list, $update = TRUE)
 		{
 			$list    = trim((string)$list);
 			$coupons = array(); // Initialize.
@@ -72,37 +72,37 @@ if(!class_exists('c_ws_plugin__s2member_pro_coupons'))
 				$_coupon_parts = $_coupon = array(); // Initialize.
 				$_coupon_parts = array_map('trim', preg_split('/\|/', $_line));
 
-				if(!($_coupon['code'] = !empty($_coupon_parts[0]) ? strtoupper(preg_replace('/[_\-]+/', '', $_coupon_parts[0])) : ''))
+				if(!($_coupon['code'] = !empty($_coupon_parts[0]) ? $this->n_code($_coupon_parts[0]) : ''))
 					continue; // Not applicable; no coupon code after sanitizing.
 
-				$_coupon['discount']            = !empty($_coupon_parts[1]) ? trim($_coupon_parts[1]) : '';
-				$_coupon['percentage_discount'] = $_coupon['discount'] && preg_match('/%/', $_coupon['discount']) ? (float)$_coupon['discount'] : 0;
-				$_coupon['flat_discount']       = $_coupon['discount'] && !preg_match('/%/', $_coupon['discount']) ? (float)$_coupon['discount'] : 0;
+				$_coupon['discount']            = !empty($_coupon_parts[1]) ? $_coupon_parts[1] : '';
+				$_coupon['percentage_discount'] = $_coupon['discount'] && preg_match('/%/', $_coupon['discount']) ? (float)$_coupon['discount'] : (float)0;
+				$_coupon['flat_discount']       = $_coupon['discount'] && !preg_match('/%/', $_coupon['discount']) ? (float)$_coupon['discount'] : (float)0;
 
 				$_active_time           = $_expires_time = '';
 				$_coupon['active_time'] = $_coupon['expires_time'] = 0;
-				if(($_coupon['dates'] = !empty($_coupon_parts[2]) ? trim($_coupon_parts[2]) : ''))
+				if(($_coupon['dates'] = !empty($_coupon_parts[2]) ? $_coupon_parts[2] : ''))
 				{
 					if(strpos($_coupon['dates'], '~') !== FALSE)
 						list($_active_time, $_expires_time) = array_map('trim', explode('~', $_coupon['dates'], 2));
-					else $_expires_time = $_coupon['dates'];
+					else $_expires_time = $_coupon['dates']; // Back compat.
 
 					if($_active_time && ($_active_time = strtotime($_active_time)))
-						$_coupon['active_time'] = $_active_time;
+						$_coupon['active_time'] = (integer)$_active_time;
 
 					if($_expires_time && ($_expires_time = strtotime($_expires_time)))
-						$_coupon['expires_time'] = $_expires_time;
+						$_coupon['expires_time'] = (integer)$_expires_time;
 				}
 				unset($_active_time, $_expires_time); // Housekeeping.
 
-				$_coupon['directive'] = !empty($_coupon_parts[3]) ? preg_replace('/_/', '-', strtolower($_coupon_parts[3])) : 'all';
-				$_coupon['directive'] = preg_match('/^(ta-only|ra-only|all)$/', $_coupon['directive']) ? $_coupon['directive'] : 'all';
+				$_coupon['directive'] = !empty($_coupon_parts[3]) && strtolower($_coupon_parts[3]) !== 'all' ? preg_replace('/_/', '-', strtolower($_coupon_parts[3])) : '';
+				$_coupon['directive'] = preg_match('/^(?:ta\-only|ra\-only)$/', $_coupon['directive']) ? $_coupon['directive'] : '';
 
-				$_coupon['singulars'] = !empty($_coupon_parts[4]) ? strtolower($_coupon_parts[4]) : 'all';
-				$_coupon['singulars'] = $_coupon['singulars'] !== 'all' ? preg_split('/,+/', trim(preg_replace('/[^0-9,]/', '', $_coupon['singulars']), ',')) : array('all');
+				$_coupon['singulars'] = !empty($_coupon_parts[4]) && strtolower($_coupon_parts[4]) !== 'all' ? $_coupon_parts[4] : '';
+				$_coupon['singulars'] = $_coupon['singulars'] ? array_map('intval', preg_split('/,+/', preg_replace('/[^0-9,]/', '', $_coupon['singulars']), NULL, PREG_SPLIT_NO_EMPTY)) : array();
 
-				$_coupon['users'] = !empty($_coupon_parts[5]) ? strtolower($_coupon_parts[5]) : 'all';
-				$_coupon['users'] = $_coupon['users'] !== 'all' ? preg_split('/,+/', trim(preg_replace('/[^0-9,]/', '', $_coupon['users']), ',')) : array('all');
+				$_coupon['users'] = !empty($_coupon_parts[5]) && strtolower($_coupon_parts[5]) !== 'all' ? $_coupon_parts[5] : '';
+				$_coupon['users'] = $_coupon['users'] ? array_map('intval', preg_split('/,+/', preg_replace('/[^0-9,]/', '', $_coupon['users']), NULL, PREG_SPLIT_NO_EMPTY)) : array();
 
 				$_coupon['max_uses'] = !empty($_coupon_parts[6]) ? (integer)$_coupon_parts[6] : 0;
 
@@ -209,10 +209,99 @@ if(!class_exists('c_ws_plugin__s2member_pro_coupons'))
 			return $list;
 		}
 
-		public function check($attr = array(), $coupon_code = '', $return = '', $process = array())
+		/**
+		 * Checks to see if a Coupon Code was supplied, and if so; what does it provide?
+		 *
+		 * @package s2Member\PayPal
+		 * @since 1.5
+		 *
+		 * @param array  $attr An array of Pro Form Attributes.
+		 * @param string $coupon_code Optional. A possible Coupon Code supplied by the Customer.
+		 * @param string $return Optional. Return type. One of `response|attr`. Defaults to `attr`.
+		 * @param array  $process Optional. An array of additional processing routines to run here.
+		 *   One or more of these values: `affiliates-1px-response|affiliates-silent-post|notifications`.
+		 *
+		 * @return array|string Original array, with prices and description modified when/if a Coupon Code is accepted.
+		 *   Or, if ``$return === 'response'``, return a string response, indicating status.
+		 */
+		public function check($attr, $coupon_code, $return = 'attr', $process = array())
 		{
 			global $wpdb; // Global DB reference.
 			/** @var $wpdb \wpdb Reference for IDEs. */
+
+			global $current_user; // Global user reference.
+			/** @var $current_user \WP_User for IDEs. */
+
+			$current_time = time(); // UTC timestamp.
+			$valid_coupon = NULL; // Defaults to a `NULL` value.
+
+			if(($coupon_code = $this->n_code($coupon_code)))
+				foreach($this->coupons as $_coupon)
+				{
+					if($coupon_code !== $_coupon['code'])
+						continue; // Not a match here.
+
+					if(!$_coupon['active_time'] || $current_time >= $_coupon['active_time'])
+						if(!$_coupon['expires_time'] || $current_time <= $_coupon['expires_time'])
+							if(!$_coupon['singulars'] || (!empty($attr['singular']) && in_array((integer)$attr['singular'], $_coupon['singulars'], TRUE)))
+								if(!$_coupon['users'] || ($current_user->ID && in_array((integer)$current_user->ID, $_coupon['users'], TRUE)))
+									if(!$_coupon['max_uses'] || $this->get_uses($_coupon['code']) < $_coupon['max_uses'])
+										$valid_coupon = $_coupon; // It's discount time! :-)
+					break; // It is either valid or it's not.
+				}
+			unset($_coupon); // Housekeeping.
+		}
+
+		public function get_uses($coupon_code)
+		{
+			if(!($coupon_code = trim((string)$coupon_code)))
+				return 0; // Not possible.
+
+			return (integer)get_option($this->uses_option_key($coupon_code));
+		}
+
+		public function update_uses($coupon_code)
+		{
+			if(!($coupon_code = trim((string)$coupon_code)))
+				return; // Not possible.
+
+			$uses_option_key = $this->uses_option_key($coupon_code);
+
+			if(($current_uses = get_option($uses_option_key)) === FALSE)
+				add_option($uses_option_key, 1, '', 'no');
+			else update_option($uses_option_key, $current_uses + 1);
+		}
+
+		public function delete_uses($coupon_code)
+		{
+			if(!($coupon_code = trim((string)$coupon_code)))
+				return; // Not possible.
+
+			delete_option($this->uses_option_key($coupon_code));
+		}
+
+		public function uses_option_key($coupon_code)
+		{
+			if(!($coupon_code = trim((string)$coupon_code)))
+				return ''; // Not possible.
+
+			return 's2member_cpc_uses_'.$this->n_code_hash($coupon_code);
+		}
+
+		public function n_code($coupon_code)
+		{
+			if(!($coupon_code = trim((string)$coupon_code)))
+				return ''; // Not possible.
+
+			return strtoupper(preg_replace('/[_\-]+/', '', $coupon_code));
+		}
+
+		public function n_code_hash($coupon_code)
+		{
+			if(!($coupon_code = trim((string)$coupon_code)))
+				return ''; // Not possible.
+
+			return hash('crc32b', $this->n_code($coupon_code)); // 8 chars.
 		}
 	}
 }
