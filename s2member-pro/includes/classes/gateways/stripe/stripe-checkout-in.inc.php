d811a3be206226546bd522353603b5eab76e898d
jaswsinc <jas@wsharks.com>
2015-09-23 14:07:00 -0800

    A side of suck for WP v4.3. See: websharks/s2member#732

--- s2member-pro/includes/classes/gateways/stripe/stripe-checkout-in.inc.php
+++ s2member-pro/includes/classes/gateways/stripe/stripe-checkout-in.inc.php
@@ -393,8 +393,10 @@ if(!class_exists('c_ws_plugin__s2member_pro_stripe_checkout_in'))
 								{
 									update_user_option($new__user_id, 'default_password_nag', $has_custom_password ? FALSE : TRUE, TRUE);
 
-									if (version_compare(get_bloginfo("version"), "4.3", ">="))
+									if (version_compare(get_bloginfo("version"), "4.3.1", ">="))
 										wp_new_user_notification($new__user_id, null, $has_custom_password ? "admin" : "both", $create_user['user_pass']);
+									else if (version_compare(get_bloginfo("version"), "4.3", ">="))
+										wp_new_user_notification($new__user_id, $has_custom_password ? "admin" : "both", $create_user['user_pass']);
 									else wp_new_user_notification($new__user_id, $create_user['user_pass']);
 
 									if(!empty($stripe_subscription_failed_charge_succeeded))
@@ -594,8 +596,10 @@ if(!class_exists('c_ws_plugin__s2member_pro_stripe_checkout_in'))
 								{
 									update_user_option($new__user_id, 'default_password_nag', $has_custom_password ? FALSE : TRUE, TRUE);
 
-									if (version_compare(get_bloginfo("version"), "4.3", ">="))
+									if (version_compare(get_bloginfo("version"), "4.3.1", ">="))
 										wp_new_user_notification($new__user_id, null, $has_custom_password ? "admin" : "both", $create_user['user_pass']);
+									else if (version_compare(get_bloginfo("version"), "4.3", ">="))
+										wp_new_user_notification($new__user_id, $has_custom_password ? "admin" : "both", $create_user['user_pass']);
 									else wp_new_user_notification($new__user_id, $create_user['user_pass']);
 
 									$ipn['s2member_stripe_proxy_return_url'] = trim(c_ws_plugin__s2member_utils_urls::remote(home_url('/?s2member_paypal_notify=1'), $ipn, array('timeout' => 20)));
