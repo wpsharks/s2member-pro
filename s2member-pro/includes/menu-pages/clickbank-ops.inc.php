95c76e2cb6c411fe6623dd4ed929f425b5440dd6
jaswsinc <jas@wsharks.com>
2015-12-02 22:06:29 -0900

    Support Authorize.Net. See: websharks/s2member#122

--- s2member-pro/includes/menu-pages/clickbank-ops.inc.php
+++ s2member-pro/includes/menu-pages/clickbank-ops.inc.php
@@ -1044,6 +1044,7 @@ if(!class_exists("c_ws_plugin__s2member_pro_menu_page_clickbank_ops"))
 			echo '</table>'."\n";
 			echo '</div>'."\n";
 			echo '</div>'."\n";
+			
 			echo '</div>'."\n";
 
 			echo '<p class="submit"><input type="submit" value="Save All Changes" /></p>'."\n";
--- s2member-pro/includes/menu-pages/paypal-ops-reminder-email.inc.php
+++ s2member-pro/includes/menu-pages/paypal-ops-reminder-email.inc.php
@@ -242,6 +242,7 @@ if(!class_exists("c_ws_plugin__s2member_pro_menu_page_paypal_ops_reminder_email"
 			echo '</table>'."\n";
 			echo '</div>'."\n";
 			echo '</div>'."\n";
+			
 			echo '</div>'."\n";
 		}
 	}
