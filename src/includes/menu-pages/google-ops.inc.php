0b9e8c2fb4f66c9e93db98040bc6fe700cea0fd6
jaswrks <jas@src.works>
2017-04-17 23:09:39 -0800

    - (s2Member/s2Member Pro) **UI Fix:** All menu page notices should be given the `notice` class and the additional `notice-[type]` class instead of the older generic `updated` and `error` classes. Fixed in this release. Related to [Issue #1034](https://github.com/websharks/s2member/issues/1034)
    - (s2Member/s2Member Pro) **UI Fix:** Plugins displaying Dashboard-wide notices using the older `updated` and `error` classes should be handled better to avoid displaying them below the s2Member header (on s2Member menu pages) and with non-default WordPress styles. See: [Issue #1034](https://github.com/websharks/s2member/issues/1034)

--- src/includes/menu-pages/google-ops.inc.php
+++ src/includes/menu-pages/google-ops.inc.php
@@ -48,6 +48,8 @@ if(!class_exists("c_ws_plugin__s2member_pro_menu_page_google_ops"))
 		{
 			echo '<div class="wrap ws-menu-page">'."\n";
 
+			echo '<div class="wp-header-end"></div>'."\n";
+
 			echo '<div class="ws-menu-page-toolbox">'."\n";
 			c_ws_plugin__s2member_menu_pages_tb::display();
 			echo '</div>'."\n";
