8c14eae5b62d886d8b1799e8d797e7cd72d0aa76
clavaque <clavaque@gmail.com>
2020-01-31 13:10:26 +0100

    Make it possible to install Pro via WP plugins manager instead of requiring FTP.

---
+++ s2member-pro.php
@@ -0,0 +1,9 @@
+<?php
+/* -- This section for WordPress parsing. ------------------------------------------------------------------------------
+
+Plugin Name: s2Member Pro
+Description: This is not a normal plugin, the activation link/status on the left is not relevant. This add-on activates automatically when its version matches the s2Member Framework's version.
+Author: WP Sharks
+Author URI: http://s2member.com/
+
+- end section for WordPress parsing. ------------------------------------------------------------------------------- */
