e76e893a921021a033b6c16509494d74a33c571a
clavaque <clavaque@gmail.com>
2020-02-03 03:02:22 +0100

    Added more Pro add-on info for WP plugin manager.

--- s2member-pro.php
+++ s2member-pro.php
@@ -3,7 +3,9 @@
 
 Plugin Name: s2Member Pro
 Description: This is not a normal plugin, the activation link/status on the left is not relevant. This add-on activates automatically when its version matches the s2Member Framework's version.
+Version: 191022
+Plugin URI: https://s2member.com
 Author: WP Sharks
-Author URI: http://s2member.com/
+Author URI: https://s2member.com
 
 - end section for WordPress parsing. ------------------------------------------------------------------------------- */
