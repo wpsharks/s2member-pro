c5af5736fd371aa434b126ae435d1242fdfef053
clavaque <clavaque@gmail.com>
2021-03-08 21:33:03 +0100

    Simplify Pro plugin description

--- s2member-pro.php
+++ s2member-pro.php
@@ -2,10 +2,11 @@
 /* -- This section for WordPress parsing. ------------------------------------------------------------------------------
 
 Plugin Name: s2Member Pro
-Description: This is not a normal plugin, the activation link/status on the left is not relevant. This add-on activates automatically when its version matches the s2Member Framework's version.
+Description: This add-on activates automatically when its version matches the s2Member Framework's version.
 Version: 210208
 Plugin URI: https://s2member.com
 Author: WP Sharks
 Author URI: https://s2member.com
+Copyright: © 2009 WP Sharks
 
 - end section for WordPress parsing. ------------------------------------------------------------------------------- */
